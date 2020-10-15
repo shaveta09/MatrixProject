6044a0c1a27b4bb7de58ebd026575f034ca0dc2b
Local <localuser@W2513000SJL3Y.SYMC.SYMANTEC.COM>
2020-10-15 09:20:35 -0700

    initial project commit

---
+++ MatrixProject-sn.al/src/HelloAgain.java
@@ -0,0 +1,15 @@
+
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Again");
+		System.out.println(numbers());
+	}
+	
+	public static int numbers()
+	{
+		return 1+3;
+	}
+
+}
